9754f913ff5340fc863b578606dff3dcb6d2c8ac
Jake Craige <jake@poeticsystems.com>
2014-12-05 11:05:22 -0600

    extract recording methods to nock-vcr-recorder add vcr.it method change API to use vcr.it and vcr.describe change name of project

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var describe = require('./lib/describe');
+var it       = require('./lib/it');
+
+module.exports = {
+  describe: describe,
+  it: it
+}
---
+++ test/_helper.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var fs     = require('fs');
+var path   = require('path');
+var assert = require('assert');
+
+global.assertCassette = function assertCassette(name) {
+  assert(fs.existsSync(cassettePath(name)),
+         'cassette "' + name + '" should exist');
+}
+
+global.assertNotCassette = function assertNotCassette(name) {
+  assert(!fs.existsSync(cassettePath(name)),
+         'cassette "' + name + '" should exist');
+}
+
+global.cassettePath = function cassettePath(name) {
+  return path.resolve(
+    path.join('cassettes', name + '.json')
+  );
+}
